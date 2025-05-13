7e9c890518bfb1d5d79cc9d3ff0bf43c9854eb17
Denisa Fleancu <101568110+DenisaFleancu26@users.noreply.github.com>
2025-05-13 20:10:03 +0300

    Add OpenAPI definition and external documentation

--- src/main/java/com/bank/banking_application/BankingApplication.java
+++ src/main/java/com/bank/banking_application/BankingApplication.java
@@ -1,9 +1,23 @@
 package com.bank.banking_application;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Banking Application",
+				description = "Spring Boot backend APIs for managing bank accounts.",
+				version = "v1.0"
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Full source code and documentation available on GitHub",
+				url = "https://github.com/DenisaFleancu26/Banking-Application---Spring-Boot"
+		)
+)
 public class BankingApplication {
 
 	public static void main(String[] args) {
