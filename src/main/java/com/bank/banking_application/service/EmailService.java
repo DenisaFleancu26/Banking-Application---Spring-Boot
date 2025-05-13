f80ded456830d34028a0a3c1044a2ffe22254665
Denisa Fleancu <101568110+DenisaFleancu26@users.noreply.github.com>
2025-05-13 13:41:52 +0300

    Implement email sending functionality

---
+++ src/main/java/com/bank/banking_application/service/EmailService.java
@@ -0,0 +1,8 @@
+package com.bank.banking_application.service;
+
+import com.bank.banking_application.dto.EmailDetails;
+
+public interface EmailService {
+
+    void sendEmailAlert(EmailDetails emailDetails);
+}
