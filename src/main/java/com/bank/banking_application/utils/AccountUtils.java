e6f8e9b0f294f17af1f4eadc9eda5f0b754f12d8
Denisa Fleancu <101568110+DenisaFleancu26@users.noreply.github.com>
2025-05-13 15:14:09 +0300

    Add balance enquiry messages/codes

--- src/main/java/com/bank/banking_application/utils/AccountUtils.java
+++ src/main/java/com/bank/banking_application/utils/AccountUtils.java
@@ -7,8 +7,15 @@ public class AccountUtils {
 
     public static final String ACCOUNT_EXISTS_CODE = "001";
     public static final String ACCOUNT_EXISTS_MESSAGE = "This user already has an account created!";
+
     public static final String ACCOUNT_CREATION_SUCCESS = "002";
     public static final String ACCOUNT_CREATION_MESSAGE = "Account has been successfully created!";
+
+    public static final String ACCOUNT_NOT_EXISTS_CODE = "003";
+    public static final String ACCOUNT_NOT_EXISTS_MESSAGE = "User with the provided account number dose not exists!";
+
+    public static final String ACCOUNT_FOUND_CODE = "004";
+    public static final String ACCOUNT_FOUND_MESSAGE = "User account found!";
 
     public static String generateAccountNumber(){
 
