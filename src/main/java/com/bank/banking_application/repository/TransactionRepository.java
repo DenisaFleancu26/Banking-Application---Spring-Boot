1ca72946b8b59c029dd7090c0d30cea820949e87
Denisa Fleancu <101568110+DenisaFleancu26@users.noreply.github.com>
2025-05-13 23:07:07 +0300

    Implement transaction saving logic

---
+++ src/main/java/com/bank/banking_application/repository/TransactionRepository.java
@@ -0,0 +1,9 @@
+package com.bank.banking_application.repository;
+
+import com.bank.banking_application.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, String> {
+}
