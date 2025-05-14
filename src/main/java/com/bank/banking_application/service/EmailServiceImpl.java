a4deb38cb384b59d55bf6a2d832e8b97971b69cf
Denisa Fleancu <101568110+DenisaFleancu26@users.noreply.github.com>
2025-05-14 17:52:40 +0300

    Add method to send account statement PDF via email

--- src/main/java/com/bank/banking_application/service/EmailServiceImpl.java
+++ src/main/java/com/bank/banking_application/service/EmailServiceImpl.java
@@ -1,13 +1,21 @@
 package com.bank.banking_application.service;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import com.bank.banking_application.dto.EmailDetails;
 
+import java.io.File;
+
+@Slf4j
 @Service
 public class EmailServiceImpl implements EmailService{
 
@@ -29,6 +37,28 @@ public class EmailServiceImpl implements EmailService{
             javaMailSender.send(mailMessage);
             System.out.println("Mail sent successfully!");
         } catch(MailException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void sendEmailWithAttachment(EmailDetails emailDetails) {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper;
+
+        try{
+            mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+            mimeMessageHelper.setFrom(senderEmail);
+            mimeMessageHelper.setTo(emailDetails.getRecipient());
+            mimeMessageHelper.setText(emailDetails.getMessageBody());
+            mimeMessageHelper.setSubject(emailDetails.getSubject());
+
+            FileSystemResource file = new FileSystemResource(new File(emailDetails.getAttachment()));
+            mimeMessageHelper.addAttachment(file.getFilename(), file);
+            javaMailSender.send(mimeMessage);
+
+            log.info("{} has been sent to user with email {}", file.getFilename(), emailDetails.getRecipient());
+        } catch (MessagingException e) {
             throw new RuntimeException(e);
         }
     }
