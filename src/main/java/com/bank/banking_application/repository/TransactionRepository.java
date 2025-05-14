9005550a229204a3be27a8c9cf7de1252552ff08
Denisa Fleancu <101568110+DenisaFleancu26@users.noreply.github.com>
2025-05-14 17:44:32 +0300

    Refactor transaction fetch to use repository method

--- src/main/java/com/bank/banking_application/repository/TransactionRepository.java
+++ src/main/java/com/bank/banking_application/repository/TransactionRepository.java
@@ -4,6 +4,10 @@ import com.bank.banking_application.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, String> {
+    List<Transaction> findByAccountNumberAndCreatedAtBetween(String accountNumber, LocalDate startDate, LocalDate endDate);
 }
--- src/main/java/com/bank/banking_application/service/BankStatement.java
+++ src/main/java/com/bank/banking_application/service/BankStatement.java
@@ -2,10 +2,11 @@ package com.bank.banking_application.service;
 
 import com.bank.banking_application.entity.Transaction;
 import com.bank.banking_application.repository.TransactionRepository;
+import com.itextpdf.text.*;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
@@ -14,15 +15,13 @@ import java.util.List;
 @AllArgsConstructor
 public class BankStatement {
 
-    @Autowired
     private TransactionRepository transactionRepository;
 
-    public List<Transaction> generateStatement(String accountNumber, String startDate, String endDate){
+    public List<Transaction> generateStatement(String accountNumber, String startDate, String endDate) throws DocumentException, IOException {
         LocalDate start = LocalDate.parse(startDate, DateTimeFormatter.ISO_DATE);
         LocalDate end = LocalDate.parse(endDate, DateTimeFormatter.ISO_DATE);
 
-        return transactionRepository.findAll().stream().filter(transaction -> transaction.getAccountNumber().equals(accountNumber))
-                .filter(transaction -> !transaction.getCreatedAt().isBefore(start))
-                .filter(transaction -> !transaction.getCreatedAt().isAfter(end)).toList();
+        return transactionRepository
+                .findByAccountNumberAndCreatedAtBetween(accountNumber, start, end);
     }
 }
